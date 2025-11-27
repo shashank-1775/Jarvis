c9d3d879b3ab466c7a6a6c646c6aff19f6ecb85f
Shashank R <sanshank.gk@gmail.com>
2025-11-27 14:11:35 +0530

    Update Javis.py
    ...

--- Javis.py
+++ Javis.py
@@ -3,7 +3,9 @@ import speech_recognition as sr
 import datetime
 import webbrowser
 import os
-
+from youtube_search import YoutubeSearch        
+import pywhatkit as kit
+from openai import OpenAI
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
@@ -45,12 +47,28 @@ def takeCommand():
 
 
 def openapp(query):
-    dict_app = {'vscode': r'C:\Users\shreyas\Desktop\Visual Studio Code.lnk',
-                'spotify': r'C:\Users\shreyas\Desktop\Spotify.lnk'}
+    dict_app = {'vscode': r'C:\Users\SSHASHANK R\Desktop\Visual Studio Code.lnk',
+                'spotify': r'C:\Users\SHASHANK R\Desktop\Spotify.lnk',
+                }
     a = query.replace('open', '').strip()
     for name, path in dict_app.items():
         if a in name:
             os.startfile(path)
+
+def chatgpt(query):
+    client = OpenAI(api_key="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")
+    question=query.replace('ask gpt','').strip()
+    if question == "":
+        question = query
+    response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": question}
+    ]
+    )
+    print("\nGPT:", response.choices[0].message["content"], "\n")
+
+
 
 
 if __name__ == "__main__":
@@ -59,7 +77,7 @@ if __name__ == "__main__":
     while True:
         query = takeCommand().lower()
 
-        if 'what is the time' in query:
+        if 'what is the time' in query or "what's the time" in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}")
             print(f"Sir, the time is {strTime}")
@@ -71,9 +89,12 @@ if __name__ == "__main__":
             webbrowser.open(url)
 
         elif 'play' in query:
+            
             video = query.replace('play', '').strip()
             speak(f'Playing {video} on YouTube')
-            webbrowser.open(f'https://www.youtube.com/results?search_query={video}')
+            results = YoutubeSearch(video, max_results=1).to_dict()
+            video_id = results[0]['id']
+            webbrowser.open(f'https://www.youtube.com/watch?v={video_id}')
 
         elif 'stop' in query or 'exit' in query or 'quit' in query or 'good night' in query:
             speak("Goodbye Sir. Have a nice day.")
@@ -83,5 +104,4 @@ if __name__ == "__main__":
             openapp(query)
 
         else:
-
-            print("No query matched")
+            chatgpt()
